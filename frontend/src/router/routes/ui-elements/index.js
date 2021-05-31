94175ab339eb751fcbf236e28206860dd70485fa
Ashish <ashish@webrevolution.in>
2021-05-31 12:12:59 +0530

    remove extra menu files

--- frontend/src/router/routes/ui-elements/index.js
+++ frontend/src/router/routes/ui-elements/index.js
@@ -1,9 +1,9 @@
-import cards from './cards'
+// import cards from './cards'
 import components from './components'
-import extensions from './extensions'
-import ui from './ui'
+// import extensions from './extensions'
+// import ui from './ui'
 import pageLayouts from './page-layouts'
 
-const data = [...cards, ...components, ...extensions, ...ui, ...pageLayouts]
+const data = [...components, ...pageLayouts]
 
 export default data
